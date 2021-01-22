2320d6d2dcbb1372bbd05c1d97ba8ef17ae3d7d3
Xavier Jodoin <xavier.jodoin@greybox.biz>
2021-01-22 15:34:55 -0500

    fix tag

--- pages/_document.js
+++ pages/_document.js
@@ -14,14 +14,26 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-JS8QKT15TF"></script>
-            <script>
+            {/* Global site tag (gtag.js) - Google Analytics */}
+            <script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=UA-163332503-1"
+            ></script>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `<!-- Global site tag (gtag.js) - Google Analytics -->
+              <script async src="https://www.googletagmanager.com/gtag/js?id=G-JS8QKT15TF"></script>
+              <script>
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-
+              
                 gtag('config', 'G-JS8QKT15TF');
-            </script>
+              </script>
+              `,
+                }}
+            />
+
         </Head>
         <body>
           <Main />
